b61ec3cfdc3d2983f54301db982aa0ad4db73ed6
Eli Watts <eliwatts13@gmail.com>
2018-10-04 05:38:34 -0600

    Commented on code.

--- hw_6/sketch.js
+++ hw_6/sketch.js
@@ -23,36 +23,48 @@ let box = {};
 //Gives windowHeight vairable a value of 700
 let widowHeight = 700;
 
-box.maxW = windowHeight;
-box.MaxH = windowHeight;
-
+//Initial program environment
 function setup(){
 
+  //Sets canvas width and height
   createCanvas( windowWidth, windowHeight );
 
+  //Sets canvas color
+  //Called only once at beginning of program to avoid animation effect
   background( 128 );
 
+  //Sets frame rate at 10 frames a second
   frameRate( 10 );
 
   }
 
 function draw(){
 
+  //Moves grid to center of canvas
   translate( windowWidth / 2, windowHeight * 0.5 );
 
+  //Rotates grid around box angle variable
   rotate( box.angle );
 
+  //Positions, draws, and colors shape
+  //Often uses self defined variables from Global Variables section
   rectMode( CENTER );
   strokeWeight( box.stroke );
   stroke( 0 );
   fill( box.r , box.g, box.b );
   rect( box.x, box.y, box.width, box.height );
 
-  box.width = map( mouseX, 0, windowWidth, box.width, box.maxW );
+  //Changes rectangle size values over time
+  box.width -= 1;
   box.height -= 1;
   box.angle += 1;
+
+  //Changes rectangle color value over time
+  //Floor function ensures randomly selected values chosen by random function are whole numbers
   box.r = floor( random( 0, 255 ) );
   box.b = floor( random( 0, 255 ) );
+
+  //Modolu operator prevents box.stroke from growing indefinately 
   box.stroke = (box.stroke + 1) % 2;
 
   }
