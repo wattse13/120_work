e9da4d8c48fbff67ccda103fb66c30a5b06d6c7c
Eli Watts <eliwatts13@gmail.com>
2018-11-14 09:50:10 -0700

    Trying out examples in empty example

--- hw_12/sketch.js
+++ hw_12/sketch.js
@@ -1,43 +1,76 @@
+//Creates new global variable
 let amoeba;
+//Creates new empty array
 let prey = [];
+//Creates new variable and gives it an initial value
+let startR = 50;
 
+//Initializes beginning environment
 function setup(){
+  //Sets canvas size to fill window size
   createCanvas( windowWidth, windowHeight );
-  for( i = 0; i < 30; i++ ){
-    prey[i] = new Hunted( random( 0, width ), random( 0, height ), random( 10, 30 ) );
+
+  //Calls new object based on Amoeba class
+  //Beginning position is middle of canvas and its beginning radius is 50
+  amoeba = new Amoeba( width / 2, height / 2, startR );
+
+  //Stores objects based on Amoeba class as elements in prey array
+  //If the value of i is less than _, a new object is added to the prey array
+    //Repreated until the value of 'i' is no longer less than _
+  for( i = 0; i < 1; i++ ){
+    //Prey ojbects are given random starting locations and a radius of 20
+    prey[i] = new Amoeba( random( 0, width ), random( 0, height ), 20 );
   }
-
-  amoeba = new Amoeba( width / 2, height / 2, 50 );
-
 }
 
+//Initializes draw block
 function draw(){
+  //Background is constantly redrawn so drawn objects appear to be animated
   background( 128 );
+
+  //Creep, generate, and starve functions from Amoeba class are applied to amoeba object
   amoeba.creep();
   amoeba.generate();
   amoeba.starve();
-  amoeba.devour();
 
-  for( i = 0; i < prey.length; i++ ){
-    prey[i].flee();
-    prey[i].populate();
-    prey[i].edgeBounce();
+  //Took this line of code from a coding train video narrated by Daniel Shiffman
+  //Applies class function to every iteration of every object element in the prey array
+  for( p of prey ){
+    //Applies flee, populate, and edgeBounce functions to every object element stored in prey array
+    p.flee();
+    p.populate();
+    p.edgeBounce();
+      //There is something currently wrong with the logic here as the amoeba does not stop growing if conditional statement is true
+      //If the function amoeba.devour returns false, then the amoeba.starve function is applied
+      if( amoeba.devour( p ) == false ){
+        amoeba.starve();
+        //Otherwise amoeba.grow function is applied
+      }else{
+        amoeba.grow();
+      }
   }
 
-}
+  //Lines 54 through 58 are another way to write lines 38 through 42
+  // for( i = 0; i < prey.length; i++ ){
+  //   prey[i].flee();
+  //   prey[i].populate();
+  //   prey[i].edgeBounce();
+  // }
 
+}
 /******************************************************************************/
 //Classes
 /******************************************************************************/
-
+//Instantiates new class
 class Amoeba{
-
-  constructor( startX, startY, startR ){
-    this.x = startX;
-    this.y = startY;
-    this.r = startR;
+  //Requires three parameters are filled with arguments when called
+  constructor( x, y, r ){
+    this.x = x;
+    this.y = y;
+    this.r = r;
+    this.runX = random( 1, 3 );
+    this.runY = random( 1, 3 );
   }
-
   creep(){
     let crawl = 5;
     if( keyIsDown( RIGHT_ARROW ) ){
@@ -51,11 +84,13 @@ class Amoeba{
     }
   }
 
-  devour(){
-    let absorb = dist( this.x, this.y, this.posX, this.posY );
-      if( absorb < this.r ){
-        this.r += 10;
-      }
+  devour(other){
+    let d = dist( this.x, this.y, other.x, other.y )
+      return ( d < this.r + other.r );
+    }
+
+  grow(){
+    this.r += 10;
   }
 
   starve(){
@@ -67,46 +102,67 @@ class Amoeba{
       this.r = 50;
     }
   }
-
   generate(){
     ellipseMode(CENTER);
     noStroke();
     fill( 'purple' );
     ellipse( this.x, this.y, this.r );
-
-  }
-
-}
-
-class Hunted{
-
-  constructor( x, y, r ){
-    this.r = r;
-    this.posX = x;
-    this.posY = y;
-    this.runX = random( 1, 3 );
-    this.runY = random( 1, 3 );
   }
 
   flee(){
-    this.posX += this.runX;
-    this.posY += this.runY;
+    this.x += this.runX;
+    this.y += this.runY;
   }
 
   edgeBounce(){
-    if( this.posX >= width || this.posX <= 0 ){
+    if( this.x >= width || this.x <= 0 ){
       this.runX = this.runX * - 1;
     }
-    if( this.posY >= height || this.posY <= 0 ){
+    if( this.y >= height || this.y <= 0 ){
       this.runY = this.runY * -1;
     }
   }
-
   populate(){
     ellipseMode(CENTER);
     noStroke();
     fill( 'yellow' );
-    ellipse( this.posX, this.posY, this.r );
+    ellipse( this.x, this.y, this.r );
   }
 
 }
+
+
+// class Hunted{
+//
+//   constructor( x, y, r ){
+//     this.r = r;
+//     this.posX = x;
+//     this.posY = y;
+//     this.runX = random( 1, 3 );
+//     this.runY = random( 1, 3 );
+//   }
+//   // flee(){
+//   //   this.posX += this.runX;
+//   //   this.posY += this.runY;
+//   // }
+//   // eaten(other){
+//   //   let d = dist( this.x, this.y, other.x, other.y );
+//   //       if( d < this.r + other.r ){
+//   //         return true;
+//   //       }else{
+//   //         return false;
+//   //       }
+//   // }
+//   // edgeBounce(){
+//   //   if( this.posX >= width || this.posX <= 0 ){
+//   //     this.runX = this.runX * - 1;
+//   //   }
+//   //   if( this.posY >= height || this.posY <= 0 ){}
+//   // }
+//   // populate(){
+//   //   ellipseMode(CENTER);
+//   //   noStroke();
+//   //   fill( 'yellow' );
+//   //   ellipse( this.posX, this.posY, this.r );
+//   // }
+// }
