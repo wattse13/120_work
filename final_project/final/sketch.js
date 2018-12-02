a7002f389e029be6542788e46325c2e1204ae07d
Eli Watts <eliwatts13@gmail.com>
2018-12-02 13:58:12 -0700

    Update sketch.js

--- final_project/final/sketch.js
+++ final_project/final/sketch.js
@@ -1,6 +1,6 @@
 let spaceShip;
 let cows = [];
-let farmer;
+let guy;
 
 function setup(){
 
@@ -10,7 +10,7 @@ function setup(){
   cows[i] = new Animal( random( 100, 600 ), random( 100, 600 ), 30 );
 }
 
-farmer = new Farmer( 200, 200, 20 );
+guy = new Farmer( 100, 200, 20 );
 
   spaceShip = new Ufo( 350, 350, 75 );
 
@@ -20,7 +20,7 @@ function draw(){
 
   background( 'black' );
 
-  fill( 128 );
+  fill( 'brown' );
   quad( 100, 100, 600, 100, 600, 600, 100, 600 )
   // quad( 100, 50, 600, 50, 700, 600, 0, 600 );
   // quad( 350, 50, 700, 200, 350, 600, 0, 200 );
@@ -37,12 +37,15 @@ function draw(){
     }
   }
 
-  farmer.patrol();
-  farmer.display();
+push();
+  guy.patrol();
+  guy.display();
+  // setTimeout( guy.turn(), 1000 );
+pop();
 
-  spaceShip.invade();
   spaceShip.fly();
   spaceShip.abductionBeam();
+  spaceShip.invade();
 
 }
 
@@ -78,8 +81,10 @@ class Ufo{
 
   abductionBeam(){
     if( mouseIsPressed ){
-      fill( 'purple' );
-      quad( this.x - 25, this.y, this.x + 25, this.y, this.x + 50, this.y + 100, this.x - 50, this.y + 100 );
+      fill( 'green' );
+      ellipseMode( CENTER );
+      ellipse( this.x, this.y, 100 );
+      // quad( this.x - 25, this.y, this.x + 25, this.y, this.x + 50, this.y + 100, this.x - 50, this.y + 100 );
     }
   }
 
@@ -148,22 +153,32 @@ class Farmer{
     this.x = x;
     this.y = y;
     this.r = r;
+    this.vis = this.vis;
+
+    this.deltaX = 2;
+    this.deltaY = 2;
   }
 
   patrol(){
+    let fence = -1;
+
     this.x += this.deltaX;
     this.y += this.deltaY;
 
     if( this.x <= 100 || this.x >= 600 ){
-      this.deltaX = this.deltaX * shock;
+      this.deltaX = this.deltaX * fence;
     }
     if( this.y <= 100 || this.y >= 600 ){
-      this.deltaY = this.deltaY * shock;
+      this.deltaY = this.deltaY * fence;
     }
   }
 
   turn(){
-    rotate( radians( 45 ) );
+
+    if( ( this.x <= 100 || this.x >= 600 ) || ( this.y <= 100 || this.y >= 600 ) ){
+      rotate( radians( 45 ) );
+      guy.display();
+    }
   }
 
   display(){
@@ -171,5 +186,7 @@ class Farmer{
     noStroke();
     fill( 'red' );
     ellipse( this.x, this.y, this.r );
+
+    this.vis = triangle( this.x, this.y, this.x + 50, this.y + 100, this.x - 50, this.y + 100 );
   }
 }
